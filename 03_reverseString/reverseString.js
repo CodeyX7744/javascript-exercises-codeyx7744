1807b006e539b81fbfb5e094464de6cabfd2edcd
codeyx <codeyx7744@gmail.com>
2024-09-22 16:41:10 +0800

    Answer exercise 2 and first test of exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let reverseWord ="";
+    for (let i=input.length-1; i>=0; i--) {
+        reverseWord += input[i];
+    }
+    return reverseWord;
 };
 
 // Do not edit below this line
