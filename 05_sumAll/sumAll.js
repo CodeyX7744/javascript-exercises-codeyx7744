e9f8b06da53911ce00e46bf67b91490c00b234a1
codeyx <codeyx7744@gmail.com>
2024-09-23 18:48:06 +0800

    answer 06_leapYears exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    let start1 = null;
+    let end1 = null;
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    } else if(!Number.isInteger(start) || !Number.isInteger(end) ) {
+        return "ERROR"; 
+    } else if (typeof start !== "number" || typeof end !== "number"){
+        return "ERROR"; 
+    } 
 
+
+    if (start > end) {
+        start1 = arguments[1];
+        end1 = arguments[0];
+    } else {
+        start1 = arguments[0];
+        end1 = arguments[1];
+    }
+    
+    for(let i=start1; i<=end1; i++) {
+        sum += i;
+    }
+    return sum;
 };
-
+//sumAll(1, 4))
 // Do not edit below this line
 module.exports = sumAll;
